56a812f5bf2a05fb60a8fbc8d192c6a273b7b942
Freddy <nglimaai01@gmail.com>
2024-10-25 10:45:55 -0600

    test: Testeando el sistema de estado

--- src/ctrl/Main.java
+++ src/ctrl/Main.java
@@ -1,7 +1,16 @@
 package ctrl;
 
+import utils.StateManager;
+
 public class Main {
+   static StateManager sm = new StateManager();
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        sm.login("c94cb914-1e18-417a-a65b-cc2cba3aaa0c");
+        System.out.println(StateManager.islogged);
+        System.out.println(sm.getCurrentSession());
+
+        // sm.logout();
+        // System.out.println(StateManager.islogged);
+        // System.out.println(sm.getCurrentSession());
     }
 }
