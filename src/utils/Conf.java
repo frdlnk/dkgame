28ff21b15bc1abf8e2b810f64a4d8a6f7ade4688
Freddy <nglimaai01@gmail.com>
2024-11-10 10:25:15 -0600

    feat: Añadiendo, la configuracion restante

--- src/utils/Conf.java
+++ src/utils/Conf.java
@@ -1,13 +1,32 @@
 package utils;
 
 public final class Conf {
-	public final static int WINDOW_HEIGT = 448;
-	public final static int WINDOW_WIDHT = 1080;
-	
+	public final static int WINDOW_HEIGHT = 448;
+	public final static int WINDOW_WIDTH = 1080;
+
 	public final static int PLAYER_HEIGHT = 80;
-	public final static int PLAYER_WIDHT = 50;
+	public final static int PLAYER_WIDTH = 50;
 
 	public final static double GRAVITY = 9.8;
-	
+
 	public final static double VELOCIDAD_CAMBIO_DE_ZONAS = 300;
+
+	public static String enemyType = "Soldados";
+	public static int currentLevel = 1;
+	public static int enemiesPerType = 10;
+	public static int extraLives = 3;
+	public static float gameSpeed = 1.0f;
+	public static String playerWeaponType = "Pistol";
+	public static float difficultyIncrease = 1.1f;
+
+	public static void showConfig() {
+		System.out.println("Configuración actual del juego:");
+		System.out.println("Tipo de enemigos: " + enemyType);
+		System.out.println("Nivel actual: " + currentLevel);
+		System.out.println("Enemigos por tipo: " + enemiesPerType);
+		System.out.println("Vidas extras: " + extraLives);
+		System.out.println("Velocidad del juego: " + gameSpeed);
+		System.out.println("Tipo de arma: " + playerWeaponType);
+		System.out.println("Aumento de dificultad: " + difficultyIncrease);
+	}
 }
