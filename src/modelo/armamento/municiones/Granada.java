d4628bda0db67338832e3727d33e22459cd926a5
LuckFreak <104868841+LuckFreak@users.noreply.github.com>
2024-11-19 01:24:56 -0600

    Create Granada.java
    se crea una granada

---
+++ src/modelo/armamento/municiones/Granada.java
@@ -0,0 +1,55 @@
+package modelo.armamento.municiones;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import modelo.armamento.Explosion;
+import modelo.armamento.Explosivo;
+import motor_v1.motor.Entidad;
+import motor_v1.motor.GameLoop;
+import motor_v1.motor.Scene;
+import motor_v1.motor.component.Renderer;
+import motor_v1.motor.util.Vector2D;
+import utils.Tags;
+import utils.arrays.ArrayString;
+import vista.escena.EscenaJuego;
+
+public class Granada extends Municion implements Explosivo {
+
+	public Granada(String nombre, Vector2D posicion, Vector2D direccion, ArrayString targetsIgnore, double dano,
+			double velocity) {
+		super(nombre, posicion, direccion, targetsIgnore, dano, velocity);
+		fisica.setGravity(1);
+	}
+
+	@Override
+	public Explosion generarExplosion() {
+		Rectangle rect = new Rectangle(30,50);
+		Color color = new Color(255,0,0,30);
+		BufferedImage texturaExplosion = Renderer.crearTextura(rect, color);
+		return new Explosion(Tags.DEADBOX, texturaExplosion, transformar, targetIgnore);
+	}
+	
+	@Override
+	public void actualizar() {
+		super.actualizar();
+	}
+
+	@Override
+	public void explotar() {
+		Scene escena = Scene.getEscenaActual();
+		if (escena instanceof EscenaJuego) {
+			((EscenaJuego) escena).addEntidad(generarExplosion());
+		}
+	}
+
+	@Override
+	protected void impacto(Entidad entidad) {
+		if (getViva()) {
+			explotar();
+			destruir();
+		}
+	}
+
+}
