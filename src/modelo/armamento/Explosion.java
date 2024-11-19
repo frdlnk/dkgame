127361a2a89daecc658af6253403b2f7b7d9f45a
LuckFreak <104868841+LuckFreak@users.noreply.github.com>
2024-11-19 01:24:40 -0600

    Update Explosion.java
    Se eliminan comentarios

--- src/modelo/armamento/Explosion.java
+++ src/modelo/armamento/Explosion.java
@@ -19,9 +19,6 @@ import utils.arrays.ArrayString;
  * @see TriggerBox
  */
 public class Explosion extends TriggerBox{
-	//private double tiempoDeExplosion;
-	//private double factorDeDanoXDistancia;
-	//private Vector2D epicentroRelativo = Vector2D.ZERO;
 	private double dano;
 	private ArrayString targetsIgnore;
 	
@@ -77,17 +74,12 @@ public class Explosion extends TriggerBox{
 	 * @param soldado Soldado al que se hara dano
 	 */
 	private void hitSoldado(Soldado soldado) {
-		//TODO: generar el epicentro y calculo de la explosion para dano dinamico segun distancia
-		//Vector2D epicentroExp = transformar.getPosicion().add(epicentroRelativo);
-		//double distanciaSoldado = epicentroRelativo.subtract(soldado.getCentro()).getMagnitude()+1;
-		//double danoConseguido = (dano/distanciaSoldado)*factorDeDanoXDistancia;
 		soldado.recibirDano(dano);
 	}
 	
 	@Override
 	public void actualizar() {
-		//TODO crear timer de duracion y controlar colision unica
-		//solo existe un segundo
+		//solo existe un frame
 		destruir();
 		super.actualizar();
 	}
