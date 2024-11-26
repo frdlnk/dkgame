baa983002e7366081ffe766657240c86dba182ee
Luck <luckfreak693@gmail.com>
2024-11-26 01:55:19 -0600

    Create EscenaGanar.java

---
+++ src/vista/escena/EscenaGanar.java
@@ -0,0 +1,24 @@
+package vista.escena;
+
+import java.awt.Graphics;
+
+import motor_v1.motor.Scene;
+
+public class EscenaGanar extends Scene {
+
+	@Override
+	public void actualizar() {
+		
+	}
+
+	@Override
+	public void destruir() {
+
+	}
+
+	@Override
+	public void dibujar(Graphics arg0) {
+
+	}
+
+}
