036cb5468d05d26faf3e9710ad9ec3ec1c470fad
Luck <luckfreak693@gmail.com>
2024-12-12 01:16:38 -0600

    Delete package-info.java

--- src/modelo/db/text/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Este paquete contiene las clases de entrada y salida para el guardado de registros de texto
- * 
- * @see modelo.db.text.ObjectFileWriter
- * @see modelo.db.text.ObjectReadManager
- */
-package modelo.db.text;
